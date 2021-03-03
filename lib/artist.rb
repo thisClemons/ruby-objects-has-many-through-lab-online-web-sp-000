4b5e0cd43deea4ac61a857db67cfa0e6e3cdb654
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-03 17:18:48 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    all_genres = songs.collect {|song| song.genre}
+    all_genres.uniq   
+  end
+
+
+
+
+end
