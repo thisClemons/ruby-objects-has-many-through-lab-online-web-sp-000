3624139b14ad675c86dd2b9ed17268806e0ad953
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-03 17:41:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,25 @@
 class Doctor
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select{|appt| appt.doctor == self}
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.collect {|appt| appt.patient}.uniq    
+  end
+
 
 end
